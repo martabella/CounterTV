a9a26f918d250431ddb7165e242302484e4a808c
martabella <mbella@lasalle.cat>
2025-11-26 16:51:58 +0100

    Initial Commit

---
+++ CounterTV/CounterTVApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterTVApp.swift
+//  CounterTV
+//
+//  Created by Marta Bella on 26/11/25.
+//
+
+import SwiftUI
+
+@main
+struct CounterTVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
